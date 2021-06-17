20f4f4059b2f267c64b3e06d742a9c2bc0989071
Naman Modani <62112480+namanmodani@users.noreply.github.com>
2021-06-17 12:22:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 @app.route('/index')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
