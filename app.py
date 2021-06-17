a9a81c382fa814bef0f7eb4c3fd9da7f910ae8e2
Naman Modani <62112480+namanmodani@users.noreply.github.com>
2021-06-17 12:11:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pickle
 from flask import Flask, render_template, request
 
 #creating instance of the class
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='templates')
 
 #to tell flask what url shoud trigger the function index()
 @app.route('/')
