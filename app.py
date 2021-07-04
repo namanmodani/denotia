6fa455719e88d117fc0106289bc5e6d63fa746fe
aranyoray <44411039+aranyoray@users.noreply.github.com>
2021-07-04 09:01:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ from flask.helpers import url_for
 import numpy as np
 import flask
 import pickle
-from flask import Flask, render_template, request, url_for
+from flask import *
+from werkzeug.utils import secure_filename
+#importing the prediction funtion from gnn code
+from gnncode import pred
+
 
 #creating instance of the class
 app = Flask(__name__, template_folder='templates')
@@ -12,15 +16,27 @@ app = Flask(__name__, template_folder='templates')
 #to tell flask what url shoud trigger the function index()
 @app.route('/')
 def index():
+    if request.method == 'POST':
+		file = request.files.get('file')
+		img_bytes = file.read()
+		prediction_name = pred(img_bytes)
+        return redirect(url_for('result',name=prediction_name))
     return render_template('index.html')
-@app.route('/result/')
-def result():
-    return render_template('result.html')
+
+#@app.route('/result/')
+@app.route('/result/', methods=['GET', 'POST'])
+def result(name):
+		return render_template('result.html',name)
+                         
 @app.route('/faq/')
 def faq():
     return render_template('faq.html')
 @app.route('/about/')
 def about():
     return render_template('about.html')
+@app.route('/ftldni/')
+def ftldni():
+    return render_template('https://cind.ucsf.edu/research/grants/frontotemporal-lobar-degeneration-neuroimaging-initiative-0')
+
 if __name__ == "__main__":
     app.run(debug=True)
